620e0cbb5c3daff7a8619249f192c9bee1163c3e
NazmulKazi18 <166075383+NazmulKazi18@users.noreply.github.com>
2025-01-28 17:38:24 -0500

    page profile finis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import de React Router
 import Accueil from "./Composants/Accueil"; // Importation de ton composant Accueil
 import './Css/App.css'; // Importation des styles CSS
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           {/* Route pour la page d'accueil */}
           <Route path="/" element={<Accueil />} /> {/* Affiche la page d'accueil (avec la carte) à la racine */}
+          <Route path="/profile" element={<Profile />}  />
         </Routes>
       </div>
     </Router>
--- frontend/src/Composants/Accueil.jsx
+++ frontend/src/Composants/Accueil.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
+import { Link } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
 import '../Css/Accueil.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -27,7 +28,7 @@ const Accueil = () => {
   const [selectedRestaurantReviews, setSelectedRestaurantReviews] = useState([]);
   const [selectedRestaurantRating, setSelectedRestaurantRating] = useState(null);
   const [restaurantTypeFilter, setRestaurantTypeFilter] = useState('');
-  const [apiKey] = useState('BomU7l-lOo83v47WpThd7TP-z6yIfgGc_IVNKd3znD0t7APSalwMwbHU2yspZyq_7CdwG--85KY-Uh25STZS8fTZQg0sy6AGPadioR3GKQLxj7jy_3Sa27EPxPyXZ3Yx');
+  const [apiKey] = useState('YOUR_API_KEY');
   const [currentPage, setCurrentPage] = useState(1); // Page courante des avis
   const [reviewsPerPage] = useState(5); // Nombre d'avis par page
   const [restaurantPhoto, setRestaurantPhoto] = useState(null); // Photo du restaurant
@@ -141,118 +142,145 @@ const Accueil = () => {
   );
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-4 p-4">
-          <h1 className="mb-4">Restaurant Finder à Montréal</h1>
-
-          <button onClick={handleGeolocation} className="btn btn-primary mb-3">Utiliser ma position actuelle</button>
-
-          <input
-            type="text"
-            className="form-control mb-3"
-            placeholder="Rechercher une adresse"
-            value={searchQuery}
-            onChange={handleSearchChange}
-            onMouseOver={handleMouseOverSearch}
-          />
-
-          {autocompleteResults.length > 0 && (
-            <ul className="list-group">
-              {autocompleteResults.map((result, index) => (
-                <li
-                  key={index}
-                  className="list-group-item list-group-item-action"
-                  onClick={() => handleRestaurantClick(result)}
-                >
-                  {result.tags.name}
-                </li>
-              ))}
+    <>
+      {/* ✅ Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">🍽️ Find A Meal</Link>
+          <button 
+            className="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">🏠 Accueil</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/profile">👤 Mon Profil</Link>
+              </li>
             </ul>
-          )}
-
-          <div className="mt-3">
-            <label htmlFor="restaurantType" className="form-label">Filtrer par type de restaurant:</label>
-            <input
-              type="text"
-              id="restaurantType"
-              className="form-control"
-              placeholder="Ex: Pizza"
-              value={restaurantTypeFilter}
-              onChange={handleTypeFilterChange}
-            />
           </div>
         </div>
+      </nav>
 
-        <div className="col-md-8">
-          <MapContainer center={userLocation} zoom={11} style={{ width: '100%', height: '935px' }} scrollWheelZoom={true} dragging={false}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-4 p-4">
+            <h1 className="mb-4">Restaurant Finder à Montréal</h1>
+
+            <button onClick={handleGeolocation} className="btn btn-primary mb-3">Utiliser ma position actuelle</button>
+
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder="Rechercher une adresse"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              onMouseOver={handleMouseOverSearch}
             />
-            <CenterMap position={selectedRestaurantPosition} />
 
-            {filteredRestaurants.map((restaurant) => (
-              <Marker
-                key={restaurant.id}
-                position={[restaurant.lat, restaurant.lon]}
-                icon={customIcon}
-                eventHandlers={{
-                  click: () => handleRestaurantClick(restaurant),
-                }}
-              >
-                <Popup>
-                  <div>
-                    <h5>{restaurant.tags.name || 'Restaurant sans nom'}</h5>
-                    {restaurantPhoto && (
-                      <img src={restaurantPhoto} alt="Restaurant" style={{ width: '100%', height: 'auto' }} />
-                    )}
-                    {selectedRestaurantRating && (
-                      <p><strong>Note:</strong> {selectedRestaurantRating}</p>
-                    )}
-                    {selectedRestaurantReviews.length > 0 ? (
-                      <div>
-                        <h6>Avis:</h6>
-                        {paginatedReviews.map((review, idx) => (
-                          <div key={idx}>
-                            <div className="d-flex align-items-center">
-                              {review.user.image_url && (
-                                <img src={review.user.image_url} alt={review.user.name} style={{ width: 30, height: 30, borderRadius: '50%' }} />
-                              )}
-                              <strong>{review.user.name}:</strong>
+            {autocompleteResults.length > 0 && (
+              <ul className="list-group">
+                {autocompleteResults.map((result, index) => (
+                  <li
+                    key={index}
+                    className="list-group-item list-group-item-action"
+                    onClick={() => handleRestaurantClick(result)}
+                  >
+                    {result.tags.name}
+                  </li>
+                ))}
+              </ul>
+            )}
+
+            <div className="mt-3">
+              <label htmlFor="restaurantType" className="form-label">Filtrer par type de restaurant:</label>
+              <input
+                type="text"
+                id="restaurantType"
+                className="form-control"
+                placeholder="Ex: Pizza"
+                value={restaurantTypeFilter}
+                onChange={handleTypeFilterChange}
+              />
+            </div>
+          </div>
+
+          <div className="col-md-8">
+            <MapContainer center={userLocation} zoom={11} style={{ width: '100%', height: '935px' }} scrollWheelZoom={true}>
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              />
+              <CenterMap position={selectedRestaurantPosition} />
+
+              {filteredRestaurants.map((restaurant) => (
+                <Marker
+                  key={restaurant.id}
+                  position={[restaurant.lat, restaurant.lon]}
+                  icon={customIcon}
+                  eventHandlers={{
+                    click: () => handleRestaurantClick(restaurant),
+                  }}
+                >
+                  <Popup>
+                    <div>
+                      <h5>{restaurant.tags.name || 'Restaurant sans nom'}</h5>
+                      {restaurantPhoto && (
+                        <img src={restaurantPhoto} alt="Restaurant" style={{ width: '100%', height: 'auto' }} />
+                      )}
+                      {selectedRestaurantRating && (
+                        <p><strong>Note:</strong> {selectedRestaurantRating}</p>
+                      )}
+                      {selectedRestaurantReviews.length > 0 ? (
+                        <div>
+                          <h6>Avis:</h6>
+                          {paginatedReviews.map((review, idx) => (
+                            <div key={idx}>
+                              <div className="d-flex align-items-center">
+                                {review.user.image_url && (
+                                  <img src={review.user.image_url} alt={review.user.name} style={{ width: 30, height: 30, borderRadius: '50%' }} />
+                                )}
+                                <strong>{review.user.name}:</strong>
+                              </div>
+                              <p>{review.text}</p>
                             </div>
-                            <p>{review.text}</p>
-                          </div>
-                        ))}
-                        {selectedRestaurantReviews.length > reviewsPerPage && (
-                          <div className="pagination">
-                            <button
-                              onClick={() => handlePaginationChange('prev')}
-                              disabled={currentPage === 1}
-                            >
-                              Précédent
-                            </button>
-                            <span>Page {currentPage}</span>
-                            <button
-                              onClick={() => handlePaginationChange('next')}
-                              disabled={currentPage * reviewsPerPage >= selectedRestaurantReviews.length}
-                            >
-                              Suivant
-                            </button>
-                          </div>
-                        )}
-                      </div>
-                    ) : (
-                      <p>Aucun avis disponible.</p>
-                    )}
-                  </div>
-                </Popup>
-              </Marker>
-            ))}
-          </MapContainer>
+                          ))}
+                          {selectedRestaurantReviews.length > reviewsPerPage && (
+                            <div className="pagination">
+                              <button
+                                onClick={() => handlePaginationChange('prev')}
+                                disabled={currentPage === 1}
+                              >
+                                Précédent
+                              </button>
+                              <span>Page {currentPage}</span>
+                              <button
+                                onClick={() => handlePaginationChange('next')}
+                                disabled={currentPage * reviewsPerPage >= selectedRestaurantReviews.length}
+                              >
+                                Suivant
+                              </button>
+                            </div>
+                          )}
+                        </div>
+                      ) : (
+                        <p>Aucun avis disponible.</p>
+                      )}
+                    </div>
+                  </Popup>
+                </Marker>
+              ))}
+            </MapContainer>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
